efc6245bb5add8ce5bb1ae0057bdfedc836089e6
samkosal <67755908+samkosal@users.noreply.github.com>
2026-04-07 11:28:57 -0700

    added a data-driven pattern

--- game.js
+++ game.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-    let score = 0;
-    let pointsPerClick = 1;
+let score = 0;
+let pointsPerClick = 1;
+
+const upgrades = [
+    {id: 1, name: "sam", cost: 10, bonus: 10},
+    {id: 2, name: "konrad", cost: 30, bonus: 50},
+    {id: 3, name: "jonus", cost: 1, bonus: 2}
+]
 
 function updateDisplay() {
 
     document.getElementById("score-display").textContent = 'Score: ' + score;
     document.getElementById("rate-display").textContent = 'Points per click:' + pointsPerClick;
-=======
-
-let score = 0;
-let pointsPerClick = 1;
-
-function updateDisplay() {
-    document.getElementById("score-display").textContent = 'Score: ' + score;
-    document.getElementById("rate-display").textContent =  'Points per click: ' + pointsPerClick;
-}
-
-document.getElementById('click-btn').addEventListener('click', addToScore);
-
-function addToScore() {
-    score += pointsPerClick;
-    updateDisplay();
->>>>>>> fcf463819693d893023b87a8eadd662be7196f99
 }
 
 document.getElementById('click-btn').addEventListener('click', addToScore);
