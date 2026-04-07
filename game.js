77c1f67e28ef7bb4543652eff0f89c6e009f6b47
konradkelly <konradky@gmail.com>
2026-04-07 11:24:19 -0700

    fix JavaScript functions

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
-score = 0;
-pointsPerClick = 1;
+    let score = 0;
+    let pointsPerClick = 1;
 
 function updateDisplay() {
-    document.getElementById("score-display").textContent =`Score: ${score}`;
-    document.getElementById("rate-display") `Points per click: ${pointsPerClick}`;
-    document.getElementById('click-btn').addEventListener(onclick, addToScore());
 
-    function addToScore() {
-        score += pointsPerClick;
-        updateDisplay();
-    }
+    document.getElementById("score-display").textContent = 'Score: ' + score;
+    document.getElementById("rate-display").textContent = 'Points per click:' + pointsPerClick;
 }
 
+document.getElementById('click-btn').addEventListener('click', addToScore);
+
+function addToScore() {
+    score += pointsPerClick;
+    updateDisplay();
+}
